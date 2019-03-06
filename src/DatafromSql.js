3958f9e9bfc8ecf7eef1d991a71719f0a887d2c9
rahulgov <44619099+rahulgov@users.noreply.github.com>
2019-03-06 15:07:32 -0800

    Update DatafromSql.js

--- src/DatafromSql.js
+++ src/DatafromSql.js
@@ -17,7 +17,7 @@ export default class DatafromSql extends Component {
     }
  
     componentDidMount(){
-         axios.get('http://35.236.58.220:3001/mysql')
+         axios.get('http://localhost:3001/mysql')
          .then(res => {
              const sqlData = res.data;
              this.setState({ sqlData });
@@ -64,7 +64,7 @@ export default class DatafromSql extends Component {
       }
   
     
-      axios.post(`http://35.236.58.220:3001/file`, formData, config )
+      axios.post(`http://localhost:3001/file`, formData, config )
       .then((response) => {
         console.log(response);
     }).catch((error) => {
